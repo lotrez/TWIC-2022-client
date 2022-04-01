ae7e94a4f0ffb2ca6ec57e6b05d58ced1261d464
Lucien <voltzlucien@gmail.com>
2022-04-01 16:08:41 +0200

    Init

---
+++ src/main/java/com/twic/twic2022client/Twic2022ClientApplication.java
@@ -0,0 +1,13 @@
+package com.twic.twic2022client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Twic2022ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Twic2022ClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/twic/twic2022client/Twic2022ClientApplicationTests.java
@@ -0,0 +1,15 @@
+package com.twic.twic2022client;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Twic2022ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
