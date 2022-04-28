cd19fea50a9d820b0aded6357044b06151abef5a
Francois Bous <franbous49@gmail.com>
2022-04-28 14:43:28 +0200

    Edit init

---
+++ src/main/java/com/twic/twic2022client/controller/EditController.java
@@ -0,0 +1,25 @@
+package com.twic.twic2022client.controller;
+
+import com.twic.twic2022client.api.ApiClient;
+import com.twic.twic2022client.api.Ville;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+public class EditController {
+
+    @GetMapping("/edit")
+    public String home(Model model) throws IOException, JSONException {
+        List<Ville> villes = ApiClient.getVilles();
+        for (Ville ville: villes) {
+            System.out.println(ville.getNomCommune());
+        }
+        model.addAttribute("villes", villes);
+        return "/edit";
+    }
+}
