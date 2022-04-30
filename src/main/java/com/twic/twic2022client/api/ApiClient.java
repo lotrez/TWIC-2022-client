5bd8facf6c5c6c857186173d9159769d80ffcf9b
Lucien <voltzlucien@gmail.com>
2022-04-30 22:25:48 +0200

    okhttp

--- src/main/java/com/twic/twic2022client/api/ApiClient.java
+++ src/main/java/com/twic/twic2022client/api/ApiClient.java
@@ -2,75 +2,42 @@ package com.twic.twic2022client.api;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.sonarsource.scanner.api.internal.shaded.okhttp.OkHttpClient;
+import org.sonarsource.scanner.api.internal.shaded.okhttp.Request;
+import org.sonarsource.scanner.api.internal.shaded.okhttp.Response;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ApiClient {
     public static List<Ville> getVilles() throws IOException {
-        URL url = new URL("http://localhost:8081/villes");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        int status = con.getResponseCode();
-        BufferedReader reader;
-        String line;
-        StringBuilder responseContent = new StringBuilder();
-        if (status >= 300) {
-            reader = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-            while ((line = reader.readLine()) != null) {
-                responseContent.append(line);
-            }
-            reader.close();
-        }
-        else {
-            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            while ((line = reader.readLine()) != null) {
-                responseContent.append(line);
-            }
-            reader.close();
-        }
-//        System.out.println("response code: " + status);
-//        System.out.println("response string: " + responseContent.toString());
+        OkHttpClient client = new OkHttpClient().newBuilder()
+            .build();
+        Request request = new Request.Builder()
+                .url("http://localhost:8081/villes")
+                .method("GET", null)
+                .build();
+        Response response = client.newCall(request).execute();
         ObjectMapper mapper = new ObjectMapper();
         List<Ville> villes;
-        villes = mapper.readValue(responseContent.toString(), new TypeReference<List<Ville>>(){});
+        villes = mapper.readValue(response.body().string(), new TypeReference<List<Ville>>(){});
         return villes;
     }
 
     public static Ville getVille(String codeCommune) throws IOException {
-        URL url = new URL("http://localhost:8081/ville?codeCommuneINSEE=" + codeCommune);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        int status = con.getResponseCode();
-        BufferedReader reader;
-        String line;
-        StringBuilder responseContent = new StringBuilder();
-        if (status >= 300) {
-            reader = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-            while ((line = reader.readLine()) != null) {
-                responseContent.append(line);
-            }
-            reader.close();
-        }
-        else {
-            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            while ((line = reader.readLine()) != null) {
-                responseContent.append(line);
-            }
-            reader.close();
-        }
-//        System.out.println("response code: " + status);
-//        System.out.println("response string: " + responseContent.toString());
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("http://localhost:8081/ville?codeCommuneINSEE=" + codeCommune)
+                .method("GET", null)
+                .build();
+        Response response = client.newCall(request).execute();
         ObjectMapper mapper = new ObjectMapper();
-        Ville ville;
-        ville = mapper.readValue(responseContent.toString(), new TypeReference<Ville>(){});
-        return ville;
+        return mapper.readValue(response.body().string(), new TypeReference<Ville>(){});
     }
 
     public static void modifierVille(
